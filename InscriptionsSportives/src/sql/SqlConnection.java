2638ddcd79391c8f593355ed3fd1db923654f402
MehdiZ93 <mehdi.zerfaoui@gmail.com>
2016-05-04 15:49:23 +0200

    nouvelle bd

--- InscriptionsSportives/src/sql/SqlConnection.java
+++ InscriptionsSportives/src/sql/SqlConnection.java
@@ -7,7 +7,7 @@ public class SqlConnection {
 	{
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/inscription", "root", "");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.100.3/inscription", "root", "root");
 			JOptionPane.showMessageDialog(null, "Connection successful");
 			return conn;
 		} catch(Exception e)
